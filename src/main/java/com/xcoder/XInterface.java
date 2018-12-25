dad34f2849f602d8ca0561381223cca438cc4e4c
chuck lee <chacukslee@gmail.com>
2018-12-25 15:09:14 +0800

    XInterface 4 common constants

---
+++ src/main/java/com/xcoder/XInterface.java
@@ -0,0 +1,16 @@
+package com.xcoder;
+
+/**
+ * @author chuck lee.
+ */
+public interface XInterface {
+    /**
+     * ISO-8859-1编码
+     */
+    String ISO_8859_1_CHAR_SET = "ISO-8859-1";
+
+    /**
+     * UTF-8编码
+     */
+    String UTF_8_CHAR_SET = "UTF-8";
+}
