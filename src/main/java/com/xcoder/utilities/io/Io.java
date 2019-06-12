3d09e464d4eefcc3c1bb09147a43a59e7dce3912
chaucks <chauckslee@gmail.com>
2019-06-12 13:52:46 +0800

    Io code.

--- src/main/java/com/xcoder/utilities/io/Io.java
+++ src/main/java/com/xcoder/utilities/io/Io.java
@@ -3,6 +3,8 @@ package com.xcoder.utilities.io;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 /**
  * IO
@@ -12,69 +14,232 @@ import java.io.OutputStream;
  */
 public class Io {
 
-    private InputStream is;
+    private static final long DEFAULT_EXPIRE = 10000L;
 
-    private OutputStream os;
+    private static final long DEFAULT_TIMEOUT = 50L;
 
-    private long expire = 10000L;
+    private static final int DEFAULT_LENGTH = 102400;
 
-    private long timeout = 50L;
-
-    private int length = 102400;
-
-    public Io(InputStream is, OutputStream os) {
-        this.is = is;
-        this.os = os;
+    /**
+     * Write interface
+     *
+     * @author Chuck Lee
+     * @date 2019-06-22
+     */
+    @FunctionalInterface
+    public interface Iw {
+        /**
+         * Io write
+         *
+         * @param buffer buffer
+         * @throws IOException IOException
+         */
+        void write(byte[] buffer) throws IOException;
     }
 
-    public Io(InputStream is, OutputStream os, long expire, long timeout, int length) {
-        this(is, os);
-        this.expire = expire;
-        this.timeout = timeout;
-        this.length = length;
+    /**
+     * Port to port
+     *
+     * @param port0 port0
+     * @param port1 port1
+     * @throws IOException IOException
+     */
+    public static void p2p(int port0, int port1) throws IOException {
+        ServerSocket serverSocket0 = new ServerSocket(port0);
+        ServerSocket serverSocket1 = new ServerSocket(port1);
+        Socket socket0 = null;
+        Socket socket1 = null;
+        for (; ; ) {
+            try {
+                socket0 = serverSocket0.accept();
+                socket1 = serverSocket1.accept();
+            } catch (Throwable t) {
+                t.printStackTrace();
+                closeSockets(socket0, socket1);
+                continue;
+            }
+            s2s(socket0, socket1);
+        }
+    }
+
+    /**
+     * Port to host
+     *
+     * @param port0 port0
+     * @param host1 host1
+     * @param port1 port1
+     * @throws IOException IOException
+     */
+    public static void p2h(int port0, String host1, int port1) throws IOException {
+        ServerSocket serverSocket0 = new ServerSocket(port0);
+        Socket socket0 = null;
+        Socket socket1 = null;
+        for (; ; ) {
+            try {
+                socket0 = serverSocket0.accept();
+                socket1 = new Socket(host1, port1);
+            } catch (Throwable t) {
+                t.printStackTrace();
+                closeSockets(socket0, socket1);
+                continue;
+            }
+            s2s(socket0, socket1);
+        }
+    }
+
+    /**
+     * Host to host
+     *
+     * @param host0 host0
+     * @param port0 port0
+     * @param host1 host1
+     * @param port1 port1
+     */
+    public static void h2h(String host0, int port0, String host1, int port1) {
+        Socket socket0 = null;
+        Socket socket1 = null;
+        for (; ; ) {
+            try {
+                socket0 = new Socket(host0, port0);
+                socket1 = new Socket(host1, port1);
+            } catch (Throwable t) {
+                t.printStackTrace();
+                closeSockets(socket0, socket1);
+                continue;
+            }
+            s2s(socket0, socket1);
+        }
+    }
+
+    public static void s2s(final Socket socket0, final Socket socket1) {
+        si2so(socket0, socket1);
+        si2so(socket1, socket0);
+        try {
+            socket1.close();
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
+        try {
+            socket0.close();
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
+    }
+
+    public static void si2so(final Socket socket0, final Socket socket1) {
+        InputStream is = null;
+        OutputStream os = null;
+        try {
+            is = socket0.getInputStream();
+            os = socket1.getOutputStream();
+            i2o(is, os);
+        } catch (Throwable t) {
+            t.printStackTrace();
+        } finally {
+            try {
+                if (null != is) {
+                    is.close();
+                }
+            } catch (Throwable t) {
+                t.printStackTrace();
+            }
+            try {
+                if (null != os) {
+                    os.close();
+                }
+            } catch (Throwable t) {
+                t.printStackTrace();
+            }
+        }
     }
 
     /**
      * Read input stream and write output stream
      *
+     * @param is InputStream
+     * @param os OutputStream
      * @throws IOException          IOException
      * @throws InterruptedException InterruptedException
      */
-    public final void readWrite() throws IOException, InterruptedException {
-        int available = available(this.is, this.expire, this.timeout, this);
-        for (int i = 0, j = available / this.length; i < j; i++) {
-            byte[] buffer = new byte[this.length];
-            this.is.read(buffer);
-            this.os.write(buffer);
+    public static void i2o(final InputStream is, final OutputStream os) throws IOException, InterruptedException {
+        i2o(is, os, DEFAULT_EXPIRE, DEFAULT_TIMEOUT, DEFAULT_LENGTH);
+    }
+
+    /**
+     * Read input stream and write output stream
+     *
+     * @param is      InputStream
+     * @param os      OutputStream
+     * @param expire  expire
+     * @param timeout timeout
+     * @param length  length
+     * @throws IOException          IOException
+     * @throws InterruptedException InterruptedException
+     */
+    public static void i2o(final InputStream is, final OutputStream os, final long expire
+            , final long timeout, final int length) throws IOException, InterruptedException {
+        read(is, expire, timeout, length, buffer -> os.write(buffer));
+        os.flush();
+    }
+
+    /**
+     * Read input stream
+     *
+     * @param is      is
+     * @param expire  expire
+     * @param timeout timeout
+     * @param length  length
+     * @param iw      callback
+     * @throws IOException          IOException
+     * @throws InterruptedException InterruptedException
+     */
+    public static void read(final InputStream is, final long expire, final long timeout
+            , final int length, final Iw iw) throws IOException, InterruptedException {
+        int available = available(is, expire, timeout);
+        for (int i = 0, j = available / length; i < j; i++) {
+            byte[] buffer = new byte[length];
+            is.read(buffer);
+            iw.write(buffer);
         }
 
-        int remain = available % this.length;
+        int remain = available % length;
         if (0 < remain) {
             byte[] buffer = new byte[remain];
-            this.os.write(buffer);
+            iw.write(buffer);
         }
-        this.os.flush();
     }
 
     /**
      * Get InputStream available.
      *
-     * @param is      is
+     * @param is      InputStream
      * @param expire  expire
      * @param timeout timeout
-     * @param monitor monitor
      * @return available
      * @throws IOException          IOException
      * @throws InterruptedException InterruptedException
      */
-    public static int available(final InputStream is, final long expire, final long timeout, final Object monitor) throws IOException, InterruptedException {
+    public static int available(final InputStream is, final long expire, final long timeout) throws IOException, InterruptedException {
         int available = is.available();
         for (long t0 = System.currentTimeMillis(); 1 > available; available = is.available()) {
             if (expire < System.currentTimeMillis() - t0) {
                 throw new RuntimeException("Waiting available time out......");
             }
-            monitor.wait(timeout);
+            Thread.currentThread().wait(timeout);
         }
         return available;
+    }
+
+    public static void closeSockets(Socket... sockets) {
+        for (Socket socket : sockets) {
+            if (null == socket) {
+                continue;
+            }
+            try {
+                socket.close();
+            } catch (Throwable t) {
+                t.printStackTrace();
+            }
+        }
     }
 }
